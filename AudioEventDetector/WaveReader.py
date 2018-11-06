4e6d1ab1e95f16e60b048f8e3a94092b970f1d7f
Konrad <kejzin@gmail.com>
2018-11-06 11:36:15 +0100

    Create class WaveReader, add links.txt

---
+++ AudioEventDetector/WaveReader.py
@@ -0,0 +1,20 @@
+import wave
+
+class WaveReader:
+
+    def wave_sample_width_channels_sample_rate_reader(self, file_path):
+        """Read basic wave parameters and return it as a tuple """
+        audio_file = wave.open(file_path, 'rb')
+        sample_width = audio_file.getsampwidth()
+        channels = audio_file.getnchannels()
+        rate = audio_file.getframerate()
+        audio_data = (file_path, sample_width, channels, rate)
+        return audio_data
+
+    def audio_data_chunk_reader(self, wave_object, chunk_size = 512):
+        """ Read audio data in chunks"""
+        while True:
+            data = wave_object.read(chunk_size)
+            if not data:
+                break
+            yield data
