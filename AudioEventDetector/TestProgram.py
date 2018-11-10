10cd7bbb1e019f1090d853dda3072d3567315a4e
Konrad <kejzin@gmail.com>
2018-11-10 14:40:04 +0100

    Test added

---
+++ AudioEventDetector/TestProgram.py
@@ -0,0 +1,20 @@
+import pytest
+from WaveReader import WaveReader
+import logging
+
+
+class TestWaveReader:
+    def test_wave_sample_width_channels_sample_rate_reader(self,
+                                                           file_path="C:\\Users\\kejzin\\Documents\\"
+                                                                      "Thesis\\Thesis\\TestFiles\\Test_wave.wav"):
+        wave_reader_obj = WaveReader()
+        wave_data = wave_reader_obj.wave_sample_width_channels_sample_rate_reader(file_path)
+        logging.info(wave_data)
+        print(wave_data)
+        assert wave_data == ("C:\\Users\\kejzin\\Documents\\Thesis\\Thesis\\TestFiles\\Test_wave.wav", 16, 1, 48000)
+        logging.info('sialala')
+
+
+if __name__ == "__main__":
+    Test = TestWaveReader()
+    Test.test_wave_sample_width_channels_sample_rate_reader()
--- AudioEventDetector/WaveReader.py
+++ AudioEventDetector/WaveReader.py
@@ -1,23 +1,22 @@
 import wave
-
+import logging
 
 class WaveReader:
-
-    @staticmethod
     def wave_sample_width_channels_sample_rate_reader(self, file_path):
         """Read basic wave parameters and return it as a tuple """
         audio_file = wave.open(file_path, 'rb')
         sample_width = audio_file.getsampwidth()
+        print(sample_width)
         channels = audio_file.getnchannels()
         rate = audio_file.getframerate()
         audio_data = (file_path, sample_width, channels, rate)
+        logging.info(audio_data)
         return audio_data
 
-    @staticmethod
-    def audio_data_chunk_reader(self, wave_object, chunk_size=12):
+    def audio_data_chunk_reader(self, wave_object, chunk_size=512):
         """ Read audio data in chunks"""
         while True:
-            data = wave_object.read(chunk_size)
+            data = wave_object.readnframes(chunk_size)
             if not data:
                 break
             yield data
